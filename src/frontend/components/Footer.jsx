a8b1ba0b48902fc9e1e278b54507529afaaeb7e6
ptiza <ptizajar@gmail.com>
2026-03-17 22:18:30 +0300

    home-page-css

--- src/frontend/components/Footer.jsx
+++ src/frontend/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../css/footer.css";
 export function Footer() {
   return (
-    <div>
-      Footer
+    <div className="footer">
+      Футер
       <Link to={"/politics"}>
         <button>Политика конфиденциальности</button>
       </Link>
--- src/frontend/components/Layout.jsx
+++ src/frontend/components/Layout.jsx
@@ -2,15 +2,16 @@ import {Header} from "./Header";
 import {Footer} from "./Footer";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import "../css/layout.css";
 function Layout() {
   return (
-    <>
+    <div className="layout">
       <Header />
-      <main>
+      <main className="main-content">
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
